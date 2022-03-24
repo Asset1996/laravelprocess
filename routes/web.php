4891c25e7a4d68e3b4da69d82f01ffa0b3ae3f87
Asset1996 <khaitbay96@gmail.com>
2022-03-24 14:58:16 +0500

    Git pull test

--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,7 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-//test234
+//test2345
 
 Route::get('/', function () {
     return view('welcome');
