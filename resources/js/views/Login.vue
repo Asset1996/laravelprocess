fb2154b6e553c4cd4a5f3b6c5d0d16ad2dbe4e10
Asset1996 <khaitbay96@gmail.com>
2022-04-21 19:21:45 +0500

    FrontEnd updates

--- resources/js/views/Login.vue
+++ resources/js/views/Login.vue
@@ -62,6 +62,7 @@ import axios from 'axios'
                 console.log('test5')
                 console.log(process.env.MIX_THIS_URL)
                 console.log(process.env.MIX_THIS_PATH)
+                console.log(process.env.MIX_TEST)
                 axios
                 .post(this.API_URL + "/auth/login", this.form, {
                     headers: {
